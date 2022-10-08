30bba8914882ff7083e568f82095fc5ba050ef4a
jul-ian <julianaviles@gmail.com>
2022-10-07 22:24:52 -0700

    Added solution 4 for chapter 2, mad lib

---
+++ chapter2/4_mad_lib.py
@@ -0,0 +1,11 @@
+"""
+Prompt user for noun, verb, adjective, adverb
+Print mad lib to console
+"""
+
+noun = input('Enter a noun: ')
+verb = input('Enter a verb: ')
+adje = input('Enter an adjective: ')
+adve = input('Enter an adverb: ')
+
+print(f"Do you {verb} your {adje} {noun} {adve}? That's hilarious!")
