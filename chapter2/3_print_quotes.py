e981b6270ba9499d6b741bc0dcbea01617da8e57
jul-ian <julianaviles@gmail.com>
2022-10-07 22:19:03 -0700

    Solution to problem 3, chapter 2 added

---
+++ chapter2/3_print_quotes.py
@@ -0,0 +1,9 @@
+# Prompt user for quote and who said it
+# Print phrase including quote
+
+quote = input('What is the quote? ')
+quote_source = input('Who said it? ')
+
+outstring = quote_source + ' says, "' + quote + '".'
+
+print(outstring)
