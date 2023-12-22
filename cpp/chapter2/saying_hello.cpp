56732dcfb46845fd665000598742eb6e10ad7eea
jul-ian <julianaviles@gmail.com>
2023-12-21 16:22:47 -0800

    Didn't work before for space separated strings, used getline to fix.

--- cpp/chapter2/saying_hello.cpp
+++ cpp/chapter2/saying_hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     cout << "What is your name? ";
     string name;
-    cin >> name;
-
+    getline(cin, name);
     cout << "Hello, " << name << ", nice to meet you!" << endl;
+
 }
