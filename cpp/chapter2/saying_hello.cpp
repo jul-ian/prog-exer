49e3dde5bee078c17d0c4c678f480abae5073b3d
jul-ian <julianaviles@gmail.com>
2023-12-21 16:20:02 -0800

    Chapter 2 exercise 1 'Saying Hello' done.

---
+++ cpp/chapter2/saying_hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "What is your name? ";
+    string name;
+    cin >> name;
+
+    cout << "Hello, " << name << ", nice to meet you!" << endl;
+}
