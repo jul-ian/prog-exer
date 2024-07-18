e7ff4bdda48cabd960761ef94bfee574d184d63b
jul-ian <julianaviles@gmail.com>
2024-07-17 22:06:09 -0700

    Started workin in c, finished first exercise in chapter 2.

--- c/2-1_saying_hello.c
+++ c/2-1_saying_hello.c
@@ -1,2 +1,19 @@
-## C code
+// 1. Saying Hello
 
+#include <stdio.h>
+#define MAX_LEN 80
+
+int main(void) {
+    int n_char = 0;
+    char name[MAX_LEN], input_char;
+    printf("What is your name? ");
+    while(((input_char = getchar()) != '\n') && n_char < MAX_LEN) {
+        name[n_char++] = input_char;
+    }
+    printf("Hello, ");
+    for(int i = 0; i < n_char; i++) {
+        printf("%c", name[i]);
+    }
+    printf(", nice to meet you!\n");
+    return 0;
+}
